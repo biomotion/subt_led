10e75df83fcff99d9062a24fe28ec35237eb4306
biomotion <bensonpuppy1997@gmail.com>
2019-08-21 18:02:47 +0200

    add rainbow

---
+++ round.py
@@ -0,0 +1,44 @@
+#!/use/bin/env python2
+
+from rpi_ws281x import PixelStrip, Color
+import argparse
+import time
+
+# LED strip configuration:
+LED_COUNT = 1         # Number of LED pixels.
+LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
+# LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 10          # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
+LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+def wheel(pos):
+    """Generate rainbow colors across 0-255 positions."""
+    if pos < 85:
+        return Color(pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return Color(255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return Color(0, pos * 3, 255 - pos * 3)
+
+if __name__ == '__main__':
+    # Process arguments
+    #parser = argparse.ArgumentParser()
+    #parser.add_argument('number', help='anchor number')
+    #args = parser.parse_args()
+
+    #print args.number
+
+    strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    # Intialize the library (must be called once before other functions).
+    strip.begin()
+    # NUM_ANCHORS = 10
+    while 1:
+        for i in range(256):
+            strip.setPixelColor(0, wheel(i))
+            strip.show()
+            time.sleep(0.01)
